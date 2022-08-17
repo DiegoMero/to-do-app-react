a9536a18977db2b7382ebeba5dc0174564254538
Diego Mero <diegomero_1999@hotmail.com>
2022-08-16 23:59:49 -0500

    Add handling events and form with react

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
@@ -5,7 +6,19 @@ import React from 'react';
 
 class TodoItem extends React.Component {
   render() {
-    return <li>{this.props.todo.title}</li>;
+    return (
+      <li>
+        <input
+          type="checkbox"
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        {this.props.todo.title}
+      </li>
+    );
   }
 }
 
