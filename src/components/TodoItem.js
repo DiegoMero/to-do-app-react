86ef29430850de920eac57d70c30f4bd29e54ea5
Diego Mero <diegomero_1999@hotmail.com>
2022-08-18 01:23:25 -0500

    Add styling to the project

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,20 +3,29 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import styles from './TodoItem.module.css';
 
 class TodoItem extends React.Component {
   render() {
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
+
+    const { completed, id, title } = this.props.todo;
+
     return (
-      <li>
+      <li className={styles.item}>
         <input
           type="checkbox"
-          checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)}
         />
-        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-          Delete
-        </button>
-        {this.props.todo.title}
+        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+        <span style={completed ? completedStyle : null}>{title}</span>
       </li>
     );
   }
